9fed633868f9abe0cc36cad1913f94382eb7ef65
vitor-risso <vitor.risso13@gmail.com>
2020-07-28 16:21:43 -0300

    inicio da categoria

--- src/components/Pages/Cadastro/categoria/index.js
+++ src/components/Pages/Cadastro/categoria/index.js
@@ -12,7 +12,19 @@ function CadastroCategoria() {
         <h1>
          Cadstro de Categoria
         </h1>
-          
+        <form>
+
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
         
         <Link to='/'>
             Ir para Home
