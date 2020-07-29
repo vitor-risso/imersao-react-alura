1b6d86aa498e88ea861039cd677c98b2a98dc2ab
vitor-risso <vitor.risso13@gmail.com>
2020-07-29 17:26:06 -0300

    adiconando formularios reutilizaveis

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import react_yellow_logo from '../../assets/img/react_yellow_logo.png';
+import pipoca_logo from '../../assets/img/pipoca_logo.png';
 import './Menu.css';
 import Button from '../components/Button';
 //import ButtonLink from '../components/ButtonLink'
@@ -9,7 +9,7 @@ function Menu () {
     return (
         <nav className = "Menu">
             <Link to= "/"> 
-            <img className="Logo" src={react_yellow_logo} alt="Reactflix logo"/>
+            <img className="Logo" src={pipoca_logo} alt="Pipocaflix logo"/>
             </Link>  
 
             <Button as={Link} className="ButtonLink" to="/cadastro/video">
--- src/components/Pages/Cadastro/categoria/index.js
+++ src/components/Pages/Cadastro/categoria/index.js
@@ -1,30 +1,131 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../PageDefault';
 import {Link} from 'react-router-dom';
+
+import FormField from '../../../FormFIeld'
 
 
 
 
 function CadastroCategoria() {
+
+    const [categorias, setCategorias] = useState([]);
+
+    const valoresIniciais ={
+      nome:'',
+      descricao:'',
+      cor:'#000',
+    }
+    const [values , setValues] = useState(valoresIniciais);
+
+    function setvalue(chave, valor){
+      setValues({
+        // chave : nome, descricao, cor
+        ...values,
+        [chave]: valor,
+      })
+
+    }
+
+
+  function handleChange(infoDosEventos) {
+    setvalue(
+
+      infoDosEventos.target.getAttribute('name'),
+      infoDosEventos.target.value
+    );
+  }
+
+
     return(
       <PageDefault>
        
         <h1>
-         Cadstro de Categoria
+         Cadstro de Categoria: {values.nome}
         </h1>
-        <form>
+        <form onSubmit={function handleSubmit(infoDosEventos){
+          infoDosEventos.preventDefault();
+          console.log('vc tentou enviar ne ');
+          setCategorias([
+            ...categorias,
+            values
+            
+          ]);
 
-          <label>
-            Nome da Categoria:
-            <input
-              type="text"
-            />
-          </label>
+          setValues(valoresIniciais)
+        }}>
+          {/* state*/}
+
+          <FormField
+            label ='Nome da Categoria: '
+            name = 'nome'
+            type="text"
+            value = {values.nome}
+            onChange ={handleChange }
+
+          />
+
+
+          <FormField 
+            label ='Descrição: '
+            name = 'descricao'
+            type="textarea"
+            value = {values.nome}
+            onChange ={handleChange }
+
+          />
+        {/*
+          <div>
+            <label>
+                Descriçaõ:
+                <textarea
+                type="text"
+                value = {values.descricao}
+                name='descricao'
+                onChange ={handleChange }
+                />
+            </label>
+
+        </div>*/}
+
+
+        <FormField 
+         label ='cor'
+         name = 'cor'
+         type="color"
+         value = {values.nome}
+         onChange ={handleChange }
+
+
+        />
+          
+        { /* <div>
+              <label>
+                Cor:
+                <input
+                type="color"
+                value = {values.cor}
+                name='cor'
+                onChange ={handleChange }
+                />
+              </label>
+          </div>*/}
 
           <button>
             Cadastrar
           </button>
         </form>
+
+        <ul>
+          {categorias.map((categorias ,indice) =>{
+            return (
+              <li key ={`${categorias}${indice}`}>
+                {categorias.nome}
+              </li>
+            )
+          } 
+          )}
+        </ul>
         
         <Link to='/'>
             Ir para Home
