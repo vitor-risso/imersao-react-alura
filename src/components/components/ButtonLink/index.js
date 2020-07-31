f897c68b7bf2de5e2091298d995722f1d781abb9
vitor-risso <vitor.risso13@gmail.com>
2020-07-31 16:38:38 -0300

    commit legal

--- src/components/components/ButtonLink/index.js
+++ src/components/components/ButtonLink/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-function ButtonLink (props) {
-    //props => {className:"O que alguém passar" href="/"}
-    console.log(props)
-    return (
-        <nav >
-          <a href={props.href} className={props.className}>
-              Novo vídeo
-          </a>     
-        </nav>
+function ButtonLink(props) {
+  // props => {className:"O que alguém passar" href="/"}
+  console.log(props);
+  return (
+    <nav>
+      <a href={props.href} className={props.className}>
+        Novo vídeo
+      </a>
+    </nav>
 
-    );
+  );
 }
 
 export default ButtonLink;
