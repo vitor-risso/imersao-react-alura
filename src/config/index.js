5089ef8389b5cf7092e5ba7716b2f4183453c5ee
vitor-risso <vitor.risso13@gmail.com>
2020-08-03 01:44:56 -0300

    corrigindo erro heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.href.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://pipocaflix.herokuapp.com/categorias';
+  : 'https://pipocaflix.herokuapp.com';
 
 export default {
   URL_BACKEND,
